7c8244d04fbe96108f820e59e88e1da8a8b06a4e
Nikola Vukotic <nikolaraca@elfak.rs>
2020-02-13 22:21:59 +0100

    v

--- servicebookmechanic/src/App.tsx
+++ servicebookmechanic/src/App.tsx
@@ -2,27 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Login from './Components/LoginRegisterComponent/loginComponent';
 import Register from './Components/LoginRegisterComponent/registerComponent';
-<<<<<<< HEAD
-import './Components/LoginRegisterComponent/loginReg.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-=======
 import MechanicComponent from './Components/MechanicComponent/mechanicComponent';
 import RegisterUser from './Components/RegisterUserComponent/registerUserComponent';
 import RegisterMechanic from './Components/RegisterMechanicComponent/registerMechanicComponent';
 
->>>>>>> 4ac93bea76ad97a34554a8683072773a9bf8e01e
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        <div className="row border rounded">
-        <Register/>
-      <Login/>
-      </div>
-=======
         <RegisterMechanic />
->>>>>>> 4ac93bea76ad97a34554a8683072773a9bf8e01e
       </header>
     </div>
   );
