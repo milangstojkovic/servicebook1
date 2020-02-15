c5bff89d35dacb3a6120a6ff4f685e16b7f2bae5
Nikola Vukotic <nikolaraca@elfak.rs>
2020-02-15 03:04:05 +0100

    Edit Record added

--- servicebookmechanic/src/App.tsx
+++ servicebookmechanic/src/App.tsx
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './Components/LoginComponent/loginComponent';
 import MechanicComponent from './Components/MechanicComponent/mechanicComponent';
 import RegisterUser from './Components/RegisterUserComponent/registerUserComponent';
 import RegisterMechanic from './Components/RegisterMechanicComponent/registerMechanicComponent';
 import HomeComponent from './Components/HomeComponent/homeComponent';
 import NavComponent from './Components/NavComponent/navComponent';
-import UserComponent from './Components/UserComponent/userComponent';
+import LoginComponent from './Components/LoginComponent/loginComponent';
+import RecordsMechanic from "./Components/RecordsMechanic/recordsMechanic";
+import EditRecord from './Components/EditRecordComponent/editRecordComponent';
 
 
 const App = () => {
-
   return (
     <div className="App">
       <nav>
         <NavComponent/>
       </nav>
       <header className="App-header">
-        <UserComponent/>
-        <button onClick={e=>localStorage.clear()}>LOGOUT</button>
+        <HomeComponent />
+        <EditRecord
+          recordId="a"/>
       </header>
     </div>
   );
 }
 
-
 export default App;
+{/* <header className="App-header">
+<UserComponent/>
+<button onClick={e=>localStorage.clear()}>LOGOUT</button>
+</header> */}
--- servicebookmechanic/src/Services/record.service.tsx
+++ servicebookmechanic/src/Services/record.service.tsx
@@ -1,23 +1,34 @@
 import { Record } from "../Models/Model";
-import axios from 'axios';
-const baseUrl = 'http://localhost:3000';
+import axios from "axios";
+const baseUrl = "http://localhost:3000";
+
+export const getRecordsService = (): Promise<Record[]> =>
+  axios
+    .get<Record[]>(`${baseUrl}/record`)
+    .then(response => response.data)
+    .catch(err => {
+      throw err;
+    });
+    
 
 
-export const getRecordsByVehicleService = (vehicleid: String): Promise<Record[]> =>
-    axios.get<Record[]>(
-        `${baseUrl}/record/${vehicleid}`
-    )
-        .then(response => response.data)
-        .catch(err => {
-            throw err
-        })
-
+export const getRecordsByVehicleService = (
+  vehicleid: String
+): Promise<Record[]> =>
+  axios
+    .get<Record[]>(`${baseUrl}/record/${vehicleid}`)
+    .then(response => response.data)
+    .catch(err => {
+      throw err;
+    });
 
 export const createRecordService = (record: any): any =>
-    axios.post(
-        `${baseUrl}/record`, record
-    )
-        .catch(err => {
-            throw err
-        })
+  axios.post(`${baseUrl}/record`, record).catch(err => {
+    throw err;
+  });
 
+export const updateRecordService = (record: any): any =>
+  axios.put(`${baseUrl}/record/${record._id}`, record);
+
+export const deleteRecordService = (recordid: string): any =>
+  axios.delete(`${baseUrl}/record/${recordid}`).then(responce=>console.log(responce.data));
